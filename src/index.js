9412d0fad812e7d78f91d7cee53bd6dce8c545dd
Rahul271297 <93571508+Rahul271297@users.noreply.github.com>
2022-02-09 01:07:10 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const route = require('./routes/route.js');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose')
